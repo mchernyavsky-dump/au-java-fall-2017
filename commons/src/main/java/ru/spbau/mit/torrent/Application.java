2f6eda439212e607f3e304be485f3be809ad0e91
misha <chemike47@gmail.com>
2017-11-27 17:20:57 +0300

    Init submodules

---
+++ commons/src/main/java/ru/spbau/mit/torrent/Application.java
@@ -0,0 +1,7 @@
+package ru.spbau.mit.torrent;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ tracker/src/main/java/ru/spbau/mit/torrent/tracker/Application.java
@@ -0,0 +1,7 @@
+package ru.spbau.mit.torrent.tracker;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
